5d04e2b637f1ff41fb6ccbf9077728644af99d83
andrerpena <andrerpena@gmail.com>
2014-08-22 19:05:56 -0300

    Initial check-out

---
+++ check-out-files.ps1
@@ -0,0 +1,3 @@
+﻿Set-Location "D:\Projetos\Fabrica\LegalOne\Main"
+set-alias tf "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\tf.exe"
+gci -Recurse -Filter "*.csproj" | %{ Write-Output $_.FullName;  tf checkout $_.FullName;  }
---
+++ replace-in-files.ps1
@@ -0,0 +1,22 @@
+﻿$replaceFrom = '<Reference Include="LegalOne\.Lib\.Common';
+$replaceTo = 'ze-common';
+$filePattern = '*.csproj';
+$location = '"D:\Projetos\Fabrica\LegalOne\Development\LegalOne';
+
+
+Set-Location $location;
+$files = Get-ChildItem -Filter $filePattern -Recurse;
+foreach($file in $files)
+{
+    $content = Get-Content $file.FullName;
+    
+    $achou1 = $content -Match $replaceFrom;
+    
+    if($achou1.Count -gt 0)
+    {
+        Write-Output 'Processing file' + $file.FullName;
+        tf checkout $file.FullName;
+        $content = $content -replace $replaceFrom, $replaceTo;
+        Set-Content -Path $file.FullName -Value $content;
+    }
+}
